f88b6c41a465e8dba0b09678999fc8ff27a3175a
SimplePrutik <dimodaktil@yandex.ru>
2020-08-29 22:54:03 +0300

    scene with control

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public float speed = 2f;
+    
+    void FixedUpdate()
+    {
+        var deltaMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * (speed * Time.deltaTime);
+        transform.localPosition += deltaMove;
+    }
+}
